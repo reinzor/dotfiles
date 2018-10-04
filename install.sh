926394afd528db1900c4baaf04c62d3f6289e247
Rein Appeldoorn <reinzor@gmail.com>
2018-10-04 12:36:01 +0200

    install qtcreator

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-source ~/.dotfiles/install-vim.sh
-source ~/.dotfiles/install-bat.sh
-source ~/.dotfiles/install-terminator.sh
-source ~/.dotfiles/install-htop.sh
 source ~/.dotfiles/install-ag.sh
+source ~/.dotfiles/install-bat.sh
 source ~/.dotfiles/install-fzf.sh
+source ~/.dotfiles/install-htop.sh
 source ~/.dotfiles/install-ipython.sh
-source ~/.dotfiles/install-sublime.sh
 source ~/.dotfiles/install-pycharm.sh
+source ~/.dotfiles/install-qtcreator.sh
+source ~/.dotfiles/install-sublime.sh
+source ~/.dotfiles/install-terminator.sh
+source ~/.dotfiles/install-vim.sh
 
 mkdir -vp ~/bin
 mkdir -vp ~/.config/git
