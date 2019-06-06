dec4aede1cd2ab3c4159165ec5591a0f4646e4c9
Rein Appeldoorn <reinzor@gmail.com>
2019-06-06 10:38:13 +0200

    Update sublime installer to latest

--- install-sublime.sh
+++ install-sublime.sh
@@ -1,5 +1,5 @@
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get install sublime-text
+DIRECTORY='/opt/sublime_text_3'
+if [ ! -d "$DIRECTORY" ]; then
+    wget https://download.sublimetext.com/sublime_text_3_build_3207_x64.tar.bz2 -O /tmp/sublime_text_3.tar.bz2
+    sudo tar -xvf /tmp/sublime_text_3.tar.bz2 -C /opt/
+fi
