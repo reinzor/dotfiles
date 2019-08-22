a79980735a7f432c4615cabd43ee38678535a0ff
Rein Appeldoorn <reinzor@gmail.com>
2019-08-22 17:00:50 +0200

    Alternative sublime and qtcreator installation for bionic

--- install-sublime.sh
+++ install-sublime.sh
@@ -1,5 +1,1 @@
-DIRECTORY='/opt/sublime_text_3'
-if [ ! -d "$DIRECTORY" ]; then
-    wget https://download.sublimetext.com/sublime_text_3_build_3207_x64.tar.bz2 -O /tmp/sublime_text_3.tar.bz2
-    sudo tar -xvf /tmp/sublime_text_3.tar.bz2 -C /opt/
-fi
+sudo snap install --classic sublime-text
