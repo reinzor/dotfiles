a9e31937ed5d9e262626699eeed176f86ad1afa9
Rein Appeldoorn <reinzor@gmail.com>
2021-03-02 09:12:05 +0100

    update qtcreator install instructions

--- install-qtcreator.sh
+++ install-qtcreator.sh
@@ -1,10 +1,13 @@
 DIRECTORY='/opt/Qt'
 if [ ! -d "$DIRECTORY" ]; then
-    wget http://download.qt.io/official_releases/online_installers/qt-unified-linux-x64-online.run -O /tmp/qt-open-source-installer
+    wget https://download.qt.io/official_releases/qtcreator/4.14/4.14.1/qt-creator-opensource-linux-x86_64-4.14.1.run -O /tmp/qt-open-source-installer
     chmod +x /tmp/qt-open-source-installer
 
     echo ""
+    echo "Use manual proxy 0.0.0.0.123 (for no account)"
+    echo ""
     echo "Choose $DIRECTORY as install directory!"
+    echo ""
     echo ""
 
     sudo /tmp/qt-open-source-installer
