3ad6611828eb72efe1de67c8302090106a0e4bdf
Rein Appeldoorn <rein.appeldoorn@nobleo.nl>
2024-07-03 13:02:26 +0200

    blender + gh copilot

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
 sudo apt-get update && sudo apt-get install -y \
   silversearcher-ag \
   bat \
@@ -8,6 +14,7 @@ sudo apt-get update && sudo apt-get install -y \
   docker.io \
   f3d \
   fzf \
+  gh \
   htop \
   ipython3 \
   mdns-scan \
@@ -18,6 +25,7 @@ sudo apt-get update && sudo apt-get install -y \
 
 sudo snap install code --classic && \
   sudo snap install clion --classic && \
+  sudo snap install blender --classic && \
   sudo snap install pycharm-community --classic && \
   sudo snap install spotify --classic && \
   sudo snap install webstorm --classic
@@ -44,3 +52,7 @@ sudo adduser `whoami` docker
 # Set-up vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Set-up gh
+gh extension install github/gh-copilot --force
+gh auth login
